618f04f91e8b9bde1b6c5e452af5def7dcc606d1
Yameen Malik <65854545+YameenMalik@users.noreply.github.com>
2023-05-30 01:36:13 +0500

    [EXC-1124] Added Stored Order Interface (#33)
    * added enums need by settlement engine
    
    * added order storage order interface
    
    * bumped version
    
    ---------
    
    Co-authored-by: YameenMalik <yameen@seed.im>

--- src/enums.ts
+++ src/enums.ts
@@ -3,3 +3,26 @@ export enum OBJECT_OWNERSHIP_STATUS {
     "OWNED" = "Owned",
     "SHARED" = "Shared"
 }
+
+
+export enum ORDER_TYPE {
+    LIMIT = "LIMIT",
+    MARKET = "MARKET",
+    STOP_LIMIT = "STOP_LIMIT",
+    STOP_MARKET = "STOP_MARKET"
+}
+
+export enum ORDER_SIDE {
+    BUY = "BUY",
+    SELL = "SELL"
+}
+
+export enum TIME_IN_FORCE {
+    IMMEDIATE_OR_CANCEL = "IOC",
+    GOOD_TILL_TIME = "GTT"
+}
+
+export enum MARGIN_TYPE {
+    ISOLATED = "ISOLATED",
+    CROSS = "CROSS" // atm exchange only supports isolated margin
+}
