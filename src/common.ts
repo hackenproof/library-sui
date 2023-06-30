33cf5b8e1ac2960459904b01c5d10badbb25855d
Yameen Malik <65854545+YameenMalik@users.noreply.github.com>
2023-06-30 07:44:43 +0500

    Removed Pino Logger (#58)
    * removed pino logger
    
    * bumped version

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,4 @@
 import { config, DotenvParseOutput } from "dotenv";
-import { pino } from "pino";
 import * as dotenvParseVariables from "dotenv-parse-variables";
 
 export function parseEnv<T>(path: string): T {
@@ -21,25 +20,3 @@ export function parseEnv<T>(path: string): T {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } as any as T;
 }
-
-/**
- * log - will be used with following methods to record the events
- ** trace - all the steps during the application execution
- ** debug - most of the steps'
- ** info - for each completed functional transaction/task.
- ** warn - out of regular events but not an error.
- ** error - when logic is broken, but it does not affect the whole application
- ** fatal - when the application is halted
- */
-const pinoLogger = pino({
-    enabled: true,
-    base: undefined,
-    level: "info",
-    formatters: {
-        level: label => {
-            return { level: label };
-        }
-    }
-});
-
-export const log = pinoLogger;
