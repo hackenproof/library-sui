2118dd3a974e757abbb20d1803605df745d4f480
Yameen Malik <65854545+YameenMalik@users.noreply.github.com>
2023-08-17 01:42:54 +0500

    Payload Signer (#74)
    * added tests
    
    * added signPayload and verifyPayload
    
    * added order signer tests
    
    * added except
    
    * added sign payload using wallet
    
    * bumped version
    
    * fixed minor error
    
    * pkg update
    
    * formatted file
    
    ---------
    
    Co-authored-by: YameenMalik <yameen@seed.im>

--- src/classes/OrderSigner.ts
+++ src/classes/OrderSigner.ts
@@ -26,7 +26,7 @@ export class OrderSigner {
         };
     }
 
-    signOrder(order: Order, keyPair?: Keypair): string {
+    public signOrder(order: Order, keyPair?: Keypair): string {
         const signer = keyPair || this.keypair;
 
         let signature: string;
@@ -86,6 +86,86 @@ export class OrderSigner {
         };
     }
 
+    public signPayload(payload: unknown, keyPair?: Keypair): string {
+        const signer = keyPair || this.keypair;
+
+        const encodedData = OrderSigner.encodePayload(payload);
+
+        let signature: string;
+
+        const keyScheme = signer.getKeyScheme();
+
+        if (keyScheme == "Secp256k1") {
+            // sign the raw data
+            signature =
+                Buffer.from(signer.signData(encodedData)).toString("hex") +
+                SIGNER_TYPES.KP_SECP256;
+        } else if (keyScheme == "ED25519") {
+            // in case of ed25519 we sign the hashed msg
+            signature =
+                Buffer.from(signer.signData(encodedData)).toString("hex") +
+                SIGNER_TYPES.KP_ED25519;
+        } else {
+            throw "Invalid wallet type";
+        }
+
+        return signature;
+    }
+
+    public async signPayloadUsingWallet(
+        payload: unknown,
+        wallet: WalletContextState
+    ): Promise<SigPK> {
+        const msgBytes = new TextEncoder().encode(JSON.stringify(payload));
+        const sigOutput = await wallet.signMessage({ message: msgBytes });
+
+        const sigPublicKey = fromSerializedSignature(sigOutput.signature);
+
+        // return signature in hex - appending 2 at the end so that when verifying off-chain and on-chain
+        // we know that this sig was generated using sui ui wallet
+        return {
+            signature:
+                Buffer.from(sigPublicKey.signature).toString("hex") +
+                SIGNER_TYPES.UI_ED25519,
+            publicKey: sigPublicKey.pubKey.toString()
+        };
+    }
+
+    private static encodePayload(payload: unknown): Uint8Array {
+        const msgBytes = new TextEncoder().encode(JSON.stringify(payload));
+
+        const intentMsg = messageWithIntent(
+            IntentScope.PersonalMessage,
+            bcs.ser(["vector", "u8"], msgBytes).toBytes()
+        );
+        const encodeData = blake2b(intentMsg, { dkLen: 32 });
+
+        return encodeData;
+    }
+
+    public static verifySignature(
+        payload: unknown,
+        signature: string,
+        publicKey: string
+    ): boolean {
+        const encodedData = OrderSigner.encodePayload(payload);
+
+        const pkBytes = base64ToUint8(publicKey);
+
+        // if last index of string is zero, the signature is generated using secp wallet
+        const char = signature.slice(-1);
+        // remove last character/index from signature
+        signature = signature.slice(0, -1);
+
+        if (char == SIGNER_TYPES.KP_SECP256) {
+            return this.verifySECP(signature, sha256(encodedData), pkBytes);
+        } else if (char == SIGNER_TYPES.KP_ED25519 || char == SIGNER_TYPES.UI_ED25519) {
+            return ed25519.verify(signature, encodedData, pkBytes);
+        }
+
+        return true;
+    }
+
     /**
      * Verifies if the given signature is correct or not using the raw order
      * @param order the order used to create the signature
@@ -97,49 +177,22 @@ export class OrderSigner {
         order: Order,
         signature: string,
         publicKey: string
-    ) {
+    ): boolean {
         const serializedOrder = OrderSigner.getSerializedOrder(order);
         const encodedOrder = new TextEncoder().encode(serializedOrder);
         const orderHash = sha256(encodedOrder);
+        const pkBytes = base64ToUint8(publicKey);
 
         // if last index of string is zero, the signature is generated using secp wallet
         const char = signature.slice(-1);
-        if (char == "0") {
-            // remove last character/index from signture
-            signature = signature.slice(0, -1);
-            const sig_r_s = secp256k1.Signature.fromCompact(signature);
-            const sig_r_s_b1 = sig_r_s.addRecoveryBit(0x1);
-            const recovered_pk_1 = sig_r_s_b1
-                .recoverPublicKey(orderHash)
-                .toRawBytes(true)
-                .toString();
+        // remove last character/index from signature
+        signature = signature.slice(0, -1);
 
-            const sig_r_s_b0 = sig_r_s.addRecoveryBit(0x0);
-            const recovered_pk_0 = sig_r_s_b0
-                .recoverPublicKey(orderHash)
-                .toRawBytes(true)
-                .toString();
-
-            const pkBytes = base64ToUint8(publicKey);
-            return (
-                pkBytes.toString() === recovered_pk_1 ||
-                pkBytes.toString() === recovered_pk_0
-            );
-        }
-        // last index 1 implies ed25519 wallet
-        else if (char == "1") {
-            // remove last character/index from signture
-            signature = signature.slice(0, -1);
-            const pkBytes = base64ToUint8(publicKey);
+        if (char == SIGNER_TYPES.KP_SECP256) {
+            return this.verifySECP(signature, orderHash, pkBytes);
+        } else if (char == SIGNER_TYPES.KP_ED25519) {
             return ed25519.verify(signature, orderHash, pkBytes);
-        }
-
-        // last index 1 implies ed25519 wallet
-        else if (char == "2") {
-            // remove last character/index from signture
-            signature = signature.slice(0, -1);
-            const pkBytes = base64ToUint8(publicKey);
-
+        } else if (char == SIGNER_TYPES.UI_ED25519) {
             const intentMsg = messageWithIntent(
                 IntentScope.PersonalMessage,
                 bcs.ser(["vector", "u8"], orderHash).toBytes()
@@ -148,6 +201,30 @@ export class OrderSigner {
 
             return ed25519.verify(signature, signedData, pkBytes);
         }
+    }
+
+    public static verifySECP(
+        signature: string,
+        data: Uint8Array,
+        publicKey: Uint8Array
+    ): boolean {
+        const sig_r_s = secp256k1.Signature.fromCompact(signature);
+        const sig_r_s_b1 = sig_r_s.addRecoveryBit(0x1);
+        const recovered_pk_1 = sig_r_s_b1
+            .recoverPublicKey(data)
+            .toRawBytes(true)
+            .toString();
+
+        const sig_r_s_b0 = sig_r_s.addRecoveryBit(0x0);
+        const recovered_pk_0 = sig_r_s_b0
+            .recoverPublicKey(data)
+            .toRawBytes(true)
+            .toString();
+
+        return (
+            publicKey.toString() === recovered_pk_1 ||
+            publicKey.toString() === recovered_pk_0
+        );
     }
 
     public static getSerializedOrder(order: Order): string {
